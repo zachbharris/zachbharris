fb62b6d22e52b796deada659b078cf153909666e
zachbharris <zachbharris94@gmail.com>
2017-06-09 17:43:28 -0500

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,5 +7,5 @@ $(function() {
 
 var console;
 console.log("%c%s", "color: black; font-size: 24px;", "Hello fellow Developer!");
-console.log('Feel free to critique my website at your plesure!');
-console.log('Tweet me @zachbharris or file a bug at the github repo :)')
+console.log('Feel free to critique my website at your pleasure!');
+console.log('Tweet me @zachbharris or file a bug at the github repo :)');
