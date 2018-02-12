2068728bfae24156c77d12f6d44df5ba48ceb451
zachbharris <zachbharris94@gmail.com>
2018-02-11 20:03:37 -0600

    Start of something new

--- assets/js/main-min.js
+++
@@ -1,1 +0,0 @@
-document.body.className+="fade-out",$(function(){$("body").removeClass("fade-out")});var console;console.log("%c%s","color: black; font-size: 24px;","Hello fellow Developer!"),console.log("Feel free to critique my website at your pleasure!"),console.log("Tweet me @zachbharris or file a bug at the github repo :)");
