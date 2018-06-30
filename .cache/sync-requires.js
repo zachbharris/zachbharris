87c64fb2a46ac9ac06b964bf955b909b9287931a
zachbharris <zachbharris94@gmail.com>
2018-06-29 20:55:10 -0500

    did this work?

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,12 +7,14 @@ exports.layouts = {
 }
 
 exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/Zach/git/zachbharris/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/Zach/git/zachbharris/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/Zach/git/zachbharris/src/pages/index.js"))
 }
 
 exports.json = {
   "layout-index.json": require("/Users/Zach/git/zachbharris/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("/Users/Zach/git/zachbharris/.cache/json/dev-404-page.json"),
   "404.json": require("/Users/Zach/git/zachbharris/.cache/json/404.json"),
   "index.json": require("/Users/Zach/git/zachbharris/.cache/json/index.json"),
   "404-html.json": require("/Users/Zach/git/zachbharris/.cache/json/404-html.json")
