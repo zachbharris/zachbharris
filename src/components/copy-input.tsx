88fbc1a3ae95b28ce7402f395e361128e99fe191
Zach Harris <hi@zachbharris.com>
2023-05-25 12:16:45 -0500

    Copy Indicator and Copy to Clipboard on Button Action Only (#9)
    * wip: convert input selection to select only, no copy
    
    * wip: add copied indicator animation

--- src/components/copy-input.tsx
+++ src/components/copy-input.tsx
@@ -1,24 +1,49 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import Icon from "./icon";
 
 export default function CopyInput() {
   const inputRef = useRef<HTMLInputElement>(null);
 
+  const [copied, setCopied] = useState(false);
+
   function onClick() {
     inputRef.current?.select();
     navigator.clipboard.writeText(inputRef.current?.value ?? "");
+
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
   }
 
   return (
-    <div className="flex flex-row gap-4 bg-zinc-700/25 rounded-lg px-4 py-2">
+    <div className="relative overflow-hidden flex flex-row gap-4 bg-zinc-700/25 rounded-lg px-4 py-2">
+      <AnimatePresence>
+        {copied ? (
+          <motion.span
+            initial={{
+              opacity: 0,
+              transform: "scale(0.5)",
+            }}
+            animate={{ opacity: 1, transform: "scale(1)" }}
+            exit={{ opacity: 0, transform: "scale(0.5)" }}
+            transition={{ duration: 0.25, type: "spring" }}
+            className="absolute right-16 top-0 bottom-0 my-auto h-full inline-flex items-center"
+          >
+            Copied
+          </motion.span>
+        ) : null}
+      </AnimatePresence>
       <input
         type="email"
         value="hi@zachbharris.com"
         className="bg-transparent flex-1 overflow-hidden outline-none"
         ref={inputRef}
-        onClick={onClick}
+        onClick={() => inputRef.current?.select()}
         readOnly
       />
       <button
