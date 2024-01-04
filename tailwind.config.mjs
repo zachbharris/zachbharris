1f5a91079aaf856dcc467180df4d7c89b6c6f472
zachbharris <zachbharris94@gmail.com>
2024-01-03 21:05:46 -0600

    better decrypt effect, mentions section, more

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,15 @@ export default {
     fontFamily: {
       mono: ['GeistMono', ...defaultTheme.fontFamily.mono],
     },
+    animation: {
+      'fade-in': 'fade-in 1.5s ease-in-out',
+    },
+    keyframes: {
+      'fade-in': {
+        '0%': { opacity: '0' },
+        '100%': { opacity: '1' },
+      }
+    }
   },
   plugins: [],
 }
