a664a84d3009f1e255f9168573e33853563eacb9
Zach Harris <zachbharris94@gmail.com>
2024-09-05 23:55:51 -0500

    making shit happen

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,13 +4,10 @@ const postsCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
-    pubDate: z.date(),
+    published: z.boolean(),
+    published_date: z.date(),
     description: z.string(),
-    author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
+    image: z.string(),
     tags: z.array(z.string()),
   }),
 });
@@ -26,30 +23,7 @@ const projectsCollection = defineCollection({
   }),
 });
 
-const keyboardCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    body: z.string(),
-    bodyURL: z.string(),
-    keycaps: z.string(),
-    keycapsURL: z.string(),
-    switches: z.string(),
-    switchesURL: z.string(),
-    pcb: z.string().optional(),
-    pcbURL: z.string().optional(),
-    plate: z.string().optional(),
-    plateURL: z.string().optional(),
-    stabs: z.string().optional(),
-    stabsURL: z.string().optional(),
-  }),
-});
-
 export const collections = {
   posts: postsCollection,
   projects: projectsCollection,
-  keyboards: keyboardCollection,
 };
