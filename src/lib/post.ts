2e1741ee691307d069329c7ba74ed6f0076c5d07
Zach Harris <zachbharris94@gmail.com>
2024-09-06 20:03:20 -0500

    notes by year, shadcn

--- src/lib/post.ts
+++ src/lib/post.ts
@@ -1,7 +1,34 @@
+import type { CollectionEntry } from "astro:content";
+
 export function formatDate(date: Date) {
   return new Intl.DateTimeFormat("en-US", {
     year: "numeric",
     month: "numeric",
     day: "numeric",
   }).format(date);
+}
+
+type Note = CollectionEntry<"posts">;
+
+export function splitNotesByYear(notes: Note[]): Record<number, Note[]> {
+  return notes.reduce((acc: Record<number, Note[]>, note) => {
+    const year = note.data.published_date.getFullYear();
+    if (!acc[year]) acc[year] = [];
+    acc[year].push(note);
+    return acc;
+  }, {});
+}
+
+export function notesInYearOrder(
+  notes: Note[],
+  order: "asc" | "desc" = "desc",
+) {
+  const notesByYear = splitNotesByYear(notes);
+  return Object.keys(notesByYear)
+    .map((year) => parseInt(year))
+    .sort((a, b) => (order === "desc" ? b - a : a - b))
+    .map((year) => ({
+      year,
+      notes: notesByYear[year],
+    }));
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,26 +2,70 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: ["class"],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      colors: {
-        background: "#0C0E0D",
-        foreground: "#F2F2F2",
-        heading: "#B9D6F2",
-      },
-      fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      },
-    },
+  	container: {
+  		center: 'true',
+  		padding: '2rem',
+  		screens: {
+  			'2xl': '1400px'
+  		}
+  	},
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			heading: '#B9D6F2',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			sans: ["Inter", ...defaultTheme.fontFamily.sans]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate")],
 };
