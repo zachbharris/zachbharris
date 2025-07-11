e719e4b1d09aaed6bf5b9a6b92e54da2fb6923ee
zachbharris <zachbharris94@gmail.com>
2025-07-11 16:14:28 -0500

    init: remove old and start fresh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
-import Footer from "@/components/footer";
-import { githubAvatarURL } from "@/utils/const";
 import "../styles/globals.css";
 
 export const metadata = {
   title: "Zach Harris",
-  description:
-    "Software Engineer by day, keyboard builder by night, and coffee\
- connoisseur all the time. Proud dad to a dog and cat who don't\
- care about my React skills.",
-  openGraph: {
-    type: "website",
-    images: [{ url: githubAvatarURL }],
-  },
-  twitter: {
-    images: [{ url: githubAvatarURL }],
-  },
+  description: "",
 };
 
 export default function RootLayout({
@@ -26,8 +14,6 @@ export default function RootLayout({
     <html suppressHydrationWarning lang="en" className="min-h-screen">
       <body className="relative bg-zinc-900 text-zinc-50 p-4 min-h-screen flex flex-col selection:bg-zinc-600/25">
         {children}
-
-        <Footer />
       </body>
     </html>
   );
